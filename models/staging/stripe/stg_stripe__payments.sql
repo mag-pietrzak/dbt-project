d8baea4a84ceda759e53fdbdb9178580f4b5ecca
Magdalena Pietrzak <magdalena.pietrzak61@gmail.com>
2025-07-27 18:57:44 +0000

    clean ups

--- models/staging/stripe/stg_stripe__payments.sql
+++ models/staging/stripe/stg_stripe__payments.sql
@@ -1,9 +1,21 @@
-select
-        id as payment_id,
-        orderid as order_id,
-        paymentmethod as payment_method,
-        status, 
-        amount/100 as amount, -- amount is stored in cents, convert it to dollars
-        created as created_at
+with source as (
 
-from {{ source('stripe', 'payment') }}
+   select * from {{ source('stripe', 'payment') }}
+
+),
+
+transformed as (
+
+    select
+            id as payment_id,
+            orderid as order_id,
+            paymentmethod as payment_method,
+            status as payment_status, 
+            amount/100 as payment_amount, -- amount is stored in cents, convert it to dollars
+            created as payment_created_at
+
+    from source
+
+)
+
+select * from transformed
